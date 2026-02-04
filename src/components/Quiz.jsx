64a4de3635064b820dc6c3c8906d83dfdc572172
Nishant Bhatnagar <Nishant.bhatnagar@q2.com>
2026-02-04 06:41:56 +0530

    added summary and shuffled the answer array

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,20 +1,35 @@
 import { useState } from 'react';
 import QUESTIONS from '../questions.js';
+import quizCompleteImg from '../assets/quiz-complete.png';
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+
+  const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;
   function handleSelectAnswer(selectedAnswer) {
     console.log(userAnswers);
     setUserAnswers((prevAnswer) => {
       return [...prevAnswer, selectedAnswer];
     });
   }
+  if (quizIsCompleted) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+  const shufffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shufffledAnswers.sort(() => {
+    Math.random() - 0.5;
+  });
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shufffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
