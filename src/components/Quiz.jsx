11c9c4a8006ffb759a5e2de16d9d7f96c751fd36
Nishant Bhatnagar <Nishant.bhatnagar@q2.com>
2026-02-04 07:16:18 +0530

    added quiz timer for each question

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import QUESTIONS from '../questions.js';
 import quizCompleteImg from '../assets/quiz-complete.png';
+import QuestionTimer from './QuizTimer.jsx';
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
 
   const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;
-  function handleSelectAnswer(selectedAnswer) {
-    console.log(userAnswers);
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer,
+  ) {
     setUserAnswers((prevAnswer) => {
       return [...prevAnswer, selectedAnswer];
     });
-  }
+  }, []);
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer],
+  );
   if (quizIsCompleted) {
     return (
       <div id="summary">
@@ -27,6 +33,11 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer
+          key={activeQuestionIndex}
+          timeout={10000}
+          onTimeout={handleSkipAnswer}
+        />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shufffledAnswers.map((answer) => (
